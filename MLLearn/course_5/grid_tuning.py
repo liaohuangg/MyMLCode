6dfbf5f9bfcba0f91119f9390dcf3c5f351e88a2
liaohuangg <liaohuangg@gmail.com>
2025-06-29 07:26:22 -0500

    HalvingGridSearchCV

--- MLLearn/course_5/grid_tuning.py
+++ MLLearn/course_5/grid_tuning.py
@@ -92,29 +92,116 @@ param_grid_simple = {
 }
 
 # 实例化随机森林模型
-reg_grid = RandomForestRegressor(random_state=1412, verbose=True, n_jobs=-1)
+reg_grid = RandomForestRegressor(random_state=1412, verbose=False, n_jobs=-1)
 
 # 设置5折交叉验证（打乱数据）
 cv_grid = KFold(n_splits=5, shuffle=True, random_state=1412)
 
 # 实例化网格搜索
-search = GridSearchCV(
-    estimator=reg_grid,
-    param_grid=param_grid_simple,          # 使用上述参数网格
-    scoring="neg_mean_squared_error",      # 评估指标为负均方误差
-    verbose=True,                          # 打印详细日志
-    cv=cv_grid,                                 # 使用定义好的交叉验证
-    n_jobs=8                              # 并行使用所有CPU核心
-)
+# search = GridSearchCV(
+#     estimator=reg_grid,
+#     param_grid=param_grid_simple,          # 使用上述参数网格
+#     scoring="neg_mean_squared_error",      # 评估指标为负均方误差
+#     verbose=True,                          # 打印详细日志
+#     cv=cv_grid,                                 # 使用定义好的交叉验证
+#     n_jobs=8                              # 并行使用所有CPU核心
+# )
 
-start = time.time()
-search.fit(X, y)  # 执行网格搜索
-end = time.time()
-print("网格搜索耗时: ", end - start)
+# start = time.time()
+# search.fit(X, y)  # 执行网格搜索
+# end = time.time()
+# print("网格搜索耗时: ", end - start)
 
-# 查看结果
-print(search.best_params_)
+# # 查看结果
+# print(search.best_params_)
 '''
 网格搜索耗时:  90.13978624343872
 {'criterion': 'squared_error', 'max_depth': 10, 'max_features': 3, 'min_impurity_decrease': np.int64(0), 'n_estimators': 45}
 '''
+# 打包成函数供后续使用
+# 评估指标RMSE
+def RMSE(cvresult, key):
+    return (abs(cvresult[key])**0.5).mean()
+
+# 计算参数空间大小
+def count_space(param):
+    no_option = 1
+    for i in param_grid_simple:
+        no_option *= len(param_grid_simple[i])
+    print(no_option)
+
+# 在最优参数上进行重新建模验证结果
+def rebuild_on_best_param(ad_reg):
+    cv = KFold(n_splits=5, shuffle=True, random_state=1412)
+    result_post_adjusted = cross_validate(ad_reg, X, y, cv=cv, scoring="neg_mean_squared_error",
+                                          return_train_score=True,
+                                          verbose=True,
+                                          n_jobs=-1)
+    print("训练RMSE:{:.3f}".format(RMSE(result_post_adjusted, "train_score")))
+    print("测试RMSE:{:.3f}".format(RMSE(result_post_adjusted, "test_score")))
+
+# rebuild_on_best_param(search.best_estimator_)
+
+# 随机网格搜索
+from sklearn.model_selection import RandomizedSearchCV
+# 定义随机搜索
+# search = RandomizedSearchCV(estimator=reg_grid
+#                            ,param_distributions=param_grid_simple
+#                            ,n_iter = 800  # 子空间的大小是全域空间的一半左右
+#                            ,scoring = "neg_mean_squared_error"
+#                            ,verbose = True
+#                            ,cv = cv
+#                            ,random_state=1412
+#                            ,n_jobs=-1)
+# 训练随机搜索评估器
+#======【TIME WARNING: 5~10min】======#
+# start = time.time()
+# search.fit(X,y)
+# print(time.time() - start)
+
+# 随机网格搜索29.885427474975586
+# 随机网格搜索建议取全域空间大小的一半
+
+# 对半网格搜索 
+'''
+优化枚举网格搜索
+1 调整搜索空间
+2 调整每次训练的数据
+比起每次使用全部数据来验证一组参数，可以考虑只带入训练数据的子集对超参数进行筛选
+条件
+任意子集的分布与全数据集分布类似
+1、首先从全数据集中无放回随机抽样出一个很小的子集 d₀，并在 d₀上验证全部参数组合的性能。根据 d₀上的验证结果，淘汰评分排在后 1/2 的那一半参数组合
+2、然后，从全数据集中再无放回抽样出一个比 d₀大一倍的子集 d₁，并在 d₁上验证剩下的那一半参数组合的性能。根据 d₁上的验证结果，淘汰评分排在后 1/2 的参数组合
+3、再从全数据集中无放回抽样出一个比 d₁大一倍的子集 d₂，并在 d₂上验证剩下 1/4 的参数组合的性能。根据 d₂上的验证结果，淘汰评分排在后 1/2 的参数组合……
+如此迭代，当参数组合只剩下一组或者剩余可用的数据不足时，停下
+
+这种模式，选出的参数，泛化能力好，并且效率高
+但是局限性在于，在最开始迭代的时候就用最小的数据集筛掉了最多的参数组合
+整体数据量必须很大
+
+aggressive_elimination参数的作用 
+双重停止机制：迭代终止条件包括：① 剩余参数组合≤1 ② 所需资源超过max_resources
+激进淘汰模式：当aggressive_elimination=True时，强制用完所有可用资源才停止，可能增加计算时间但结果更可靠
+默认策略：参数默认为False，允许提前终止（当任一停止条件触发时），适合大型参数空间的快速筛选
+
+样本量不够了，使用开始用的样本重复，来筛选参数
+'''
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingGridSearchCV
+# 初始化 HalvingGridSearchCV
+halving_search = HalvingGridSearchCV(
+    estimator=reg_grid,
+    param_grid=param_grid_simple
+    ,scoring = "neg_mean_squared_error"
+    ,verbose = False
+    ,cv = cv
+    ,random_state=1412
+    ,n_jobs=-1
+    ,factor=1.5 # 每次迭代 样本量倍数
+    ,min_resources=500 # 开局使用的样本量
+)
+print("\nHalvingGridSearchCV\n")
+start = time.time()
+halving_search.fit(X,y)
+print(time.time() - start)
+rebuild_on_best_param(halving_search.best_estimator_)
